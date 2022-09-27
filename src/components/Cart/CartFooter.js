ffc0c2e927587fa534609a5ab8752c5d996d65b5
Jonghwan-choi <53891983+Jonghwan-choi@users.noreply.github.com>
2022-09-27 11:34:22 +0900

    Update CartFooter.js

--- src/components/Cart/CartFooter.js
+++ src/components/Cart/CartFooter.js
@@ -1,14 +1,11 @@
-import { number } from 'prop-types';
 import CartTotal from './CartTotal';
+import { useCart } from '../../contexts/cart'
 
-const CartFooter = ({ total, ...restProps }) => (
-  <footer {...restProps}>
-    <CartTotal>{total}</CartTotal>
-  </footer>
-);
-
-CartFooter.propTypes = {
-  total: number.isRequired,
-};
+const CartFooter = ({ ...restProps }) => {
+  const { totalPrice } = useCart();
+  return (<footer {...restProps}>
+    <CartTotal>{totalPrice}</CartTotal>
+    </footer>
+)};
 
 export default CartFooter;
